f7ea5f9196cc92fdf27674ebf6b01a18f87ebcaf
Сергей <36310292+pricklly@users.noreply.github.com>
2023-01-16 00:08:59 +0300

    monolit men
    very good

---
+++ DivingShop/DivingShop/MainForm.Designer.cs
@@ -0,0 +1,356 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: pricklly
+ * Дата: 15.01.2023
+ * Время: 23:53
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+namespace DivingShop
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Designer variable used to keep track of non-visual components.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.RichTextBox richTextBox1;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.TextBox textBox4;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.CheckBox checkBox2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.TrackBar trackBar1;
+		private System.Windows.Forms.RichTextBox richTextBox2;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.TextBox textBox5;
+		private System.Windows.Forms.Button button5;
+		
+		/// <summary>
+		/// Disposes resources used by the form.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing) {
+				if (components != null) {
+					components.Dispose();
+				}
+			}
+			base.Dispose(disposing);
+		}
+		
+		/// <summary>
+		/// This method is required for Windows Forms designer support.
+		/// Do not change the method contents inside the source code editor. The Forms designer might
+		/// not be able to load this method if it was changed manually.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel2 = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.trackBar1 = new System.Windows.Forms.TrackBar();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+			this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.checkBox2 = new System.Windows.Forms.CheckBox();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.textBox5 = new System.Windows.Forms.TextBox();
+			this.button5 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.panel2.SuspendLayout();
+			this.panel3.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+			this.panel4.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.panel4);
+			this.panel1.Controls.Add(this.progressBar1);
+			this.panel1.Controls.Add(this.textBox4);
+			this.panel1.Controls.Add(this.textBox3);
+			this.panel1.Controls.Add(this.pictureBox1);
+			this.panel1.Location = new System.Drawing.Point(1, 1);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(236, 181);
+			this.panel1.TabIndex = 0;
+			// 
+			// panel2
+			// 
+			this.panel2.Controls.Add(this.checkBox2);
+			this.panel2.Controls.Add(this.textBox2);
+			this.panel2.Controls.Add(this.pictureBox2);
+			this.panel2.Location = new System.Drawing.Point(335, 2);
+			this.panel2.Name = "panel2";
+			this.panel2.Size = new System.Drawing.Size(265, 179);
+			this.panel2.TabIndex = 1;
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.pictureBox3);
+			this.panel3.Location = new System.Drawing.Point(177, 189);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(224, 146);
+			this.panel3.TabIndex = 2;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+			this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(236, 178);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 0;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+			this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(264, 176);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 0;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+			this.pictureBox3.Location = new System.Drawing.Point(0, 3);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(223, 142);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 0;
+			this.pictureBox3.TabStop = false;
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(406, 192);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(92, 82);
+			this.listBox1.TabIndex = 3;
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(328, 14);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(57, 20);
+			this.textBox1.TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(-7, 68);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(70, 20);
+			this.textBox2.TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(8, 110);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(71, 20);
+			this.textBox3.TabIndex = 1;
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(8, 148);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(75, 20);
+			this.textBox4.TabIndex = 2;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(504, 204);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(74, 42);
+			this.button1.TabIndex = 5;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(505, 257);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(72, 41);
+			this.button2.TabIndex = 6;
+			this.button2.Text = "button2";
+			this.button2.UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(406, 280);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(80, 41);
+			this.button3.TabIndex = 7;
+			this.button3.Text = "button3";
+			this.button3.UseVisualStyleBackColor = true;
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.progressBar1.Location = new System.Drawing.Point(193, 13);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(117, 23);
+			this.progressBar1.TabIndex = 3;
+			this.progressBar1.Value = 15;
+			// 
+			// trackBar1
+			// 
+			this.trackBar1.Location = new System.Drawing.Point(277, 57);
+			this.trackBar1.Name = "trackBar1";
+			this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+			this.trackBar1.Size = new System.Drawing.Size(45, 125);
+			this.trackBar1.TabIndex = 4;
+			// 
+			// panel4
+			// 
+			this.panel4.Controls.Add(this.richTextBox1);
+			this.panel4.Location = new System.Drawing.Point(176, 56);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(70, 37);
+			this.panel4.TabIndex = 4;
+			// 
+			// richTextBox1
+			// 
+			this.richTextBox1.Location = new System.Drawing.Point(0, 0);
+			this.richTextBox1.Name = "richTextBox1";
+			this.richTextBox1.Size = new System.Drawing.Size(54, 34);
+			this.richTextBox1.TabIndex = 0;
+			this.richTextBox1.Text = "";
+			// 
+			// richTextBox2
+			// 
+			this.richTextBox2.Location = new System.Drawing.Point(9, 184);
+			this.richTextBox2.Name = "richTextBox2";
+			this.richTextBox2.Size = new System.Drawing.Size(150, 90);
+			this.richTextBox2.TabIndex = 8;
+			this.richTextBox2.Text = "";
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(94, 285);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(65, 40);
+			this.button4.TabIndex = 9;
+			this.button4.Text = "button4";
+			this.button4.UseVisualStyleBackColor = true;
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.Location = new System.Drawing.Point(9, 285);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(71, 24);
+			this.checkBox1.TabIndex = 10;
+			this.checkBox1.Text = "checkBox1";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// checkBox2
+			// 
+			this.checkBox2.Location = new System.Drawing.Point(161, 126);
+			this.checkBox2.Name = "checkBox2";
+			this.checkBox2.Size = new System.Drawing.Size(81, 24);
+			this.checkBox2.TabIndex = 6;
+			this.checkBox2.Text = "checkBox2";
+			this.checkBox2.UseVisualStyleBackColor = true;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+			this.pictureBox4.Location = new System.Drawing.Point(509, 304);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(91, 68);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox4.TabIndex = 11;
+			this.pictureBox4.TabStop = false;
+			// 
+			// textBox5
+			// 
+			this.textBox5.Location = new System.Drawing.Point(201, 341);
+			this.textBox5.Name = "textBox5";
+			this.textBox5.Size = new System.Drawing.Size(200, 20);
+			this.textBox5.TabIndex = 12;
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(406, 330);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(80, 39);
+			this.button5.TabIndex = 13;
+			this.button5.Text = "button5";
+			this.button5.UseVisualStyleBackColor = true;
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(627, 381);
+			this.Controls.Add(this.button5);
+			this.Controls.Add(this.textBox5);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.richTextBox2);
+			this.Controls.Add(this.trackBar1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.panel3);
+			this.Controls.Add(this.panel2);
+			this.Controls.Add(this.panel1);
+			this.Name = "MainForm";
+			this.Text = "DivingShop";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.panel2.ResumeLayout(false);
+			this.panel2.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+			this.panel4.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+	}
+}
---
+++ DivingShop/DivingShop/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Создано в SharpDevelop.
+ * Пользователь: pricklly
+ * Дата: 15.01.2023
+ * Время: 23:53
+ * 
+ * Для изменения этого шаблона используйте меню "Инструменты | Параметры | Кодирование | Стандартные заголовки".
+ */
+using System;
+using System.Windows.Forms;
+
+namespace DivingShop
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
